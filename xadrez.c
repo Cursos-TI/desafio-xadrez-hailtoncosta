a542a5810c51b6dc38bd1dd8b0560324b6749919
Hailton Costa <59211869+hailtoncosta@users.noreply.github.com>
2025-07-26 02:27:40 +0000

    xadrez nivel avançado

--- xadrez.c
+++ xadrez.c
@@ -6,6 +6,47 @@
 #define CASAS_CAVALO_DIREITA 2
 #define CASAS_CAVALO_ESQUERDA 1
 
+void movimentar_cavalo() {
+    // Implementação da movimentação do Cavalo
+    for (int i = 1; i <= CASAS_CAVALO_DIREITA; i++) {
+                    //2 Casas para cima
+                    printf("Cima\n");
+                    // O loop aninhado para o "Esquerda" é ativado após o último "direita"
+                    if (i == CASAS_CAVALO_DIREITA) {
+                        int j = i;
+                        while (j <= CASAS_CAVALO_DIREITA) {
+                            printf("Direita\n");
+                            j++;
+                        }
+                    }
+                }
+}
+
+void movimentar_bispo() {
+    // Implementação da movimentação do Bispo
+    int passo_bispo = 1;
+                while (passo_bispo <= CASAS_BISPO) {
+                    printf("Passo %d: Cima, Direita\n", passo_bispo);
+                    passo_bispo++;
+                }
+}
+
+void movimentar_torre() {
+    // Implementação da movimentação da Torre
+    for (int i = 1; i <= CASAS_TORRE; i++) {
+                    printf("Passo %d Direita\n", i);
+                }
+}
+
+void movimentar_rainha() {
+    // Implementação da movimentação da Rainha
+    int passo_rainha = 1;
+                do {
+                    printf("Passo %d: Esquerda\n", passo_rainha);
+                    passo_rainha++;
+                } while (passo_rainha <= CASAS_RAINHA);
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -38,46 +79,25 @@ int main() {
         {   //movimento da peça Torre (5 casas para direita)
             case 1:
                 printf("Movimento da Torre (5 casas para Direita)\n");
-                for (int i = 1; i <= CASAS_TORRE; i++) {
-                    printf("Passo %d Direita\n", i);
-                }
+                movimentar_torre();
                 break;
             
             //movimento da peça Bispo (5 casa diagonal: Cima, Direita)
             case 2:
                 printf("Movimento do Bispo (5 casas na diagonal Cima, Direita:) \n");
-                int passo_bispo = 1;
-                while (passo_bispo <= CASAS_BISPO) {
-                    printf("Passo %d: Cima, Direita\n", passo_bispo);
-                    passo_bispo++;
-                }
+                movimentar_bispo();
                 break;
             
             //Movimento da peça Rainha
             case 3:
                 printf("Movimento da Rainha (8 casas para Esquerda): \n");
-                int passo_rainha = 1;
-                do {
-                    printf("Passo %d: Esquerda\n", passo_rainha);
-                    passo_rainha++;
-                } while (passo_rainha <= CASAS_RAINHA);
+                movimentar_rainha();
                 break;
 
             case 4:
-                printf("Movimento do Cavalo (2 para baixo e 1 para esquerda): \n");
+                printf("Movimento do Cavalo (2 para cima e 1 para direita): \n");
                 //Loop externo para as 2 casas para baixo
-                for (int i = 1; i <= CASAS_CAVALO_DIREITA; i++) {
-                    //2 Casas para direita
-                    printf("Baixo\n");
-                    // O loop aninhado para o "Esquerda" é ativado após o último "Baixo"
-                    if (i == CASAS_CAVALO_DIREITA) {
-                        int j = i;
-                        while (j <= CASAS_CAVALO_DIREITA) {
-                            printf("Esquerda\n");
-                            j++;
-                        }
-                    }
-                }
+                movimentar_cavalo();
                 
                 break;
 
